3c03076f499212b7c2bff8719eb2f8228f8406cb
feb <fe.bld.dev@laposte.net>
2024-05-17 17:06:16 +0200

    feat: ajout code pour notification par event

---
+++ spring-modulith/src/main/java/feb/tutos/spring/modulith/notification/NotificationDTO.java
@@ -0,0 +1,20 @@
+package feb.tutos.spring.modulith.notification;
+
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+
+import feb.tutos.spring.modulith.notification.interne.NotificationType;
+
+public class NotificationDTO {
+
+    @Getter @Setter private Date   date;
+    @Getter @Setter private String nomProduit;
+    @Getter @Setter private String format;
+
+    public NotificationDTO( Date date, String format, String nomProduit ) {
+        this.date       = date;
+        this.format     = format;
+        this.nomProduit = nomProduit;
+    }
+}
